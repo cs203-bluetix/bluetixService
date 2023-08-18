472d06e186e15330dba2e38cc78519666ccba68f
Timothy Koh Yu Jie <82047680+findtimo@users.noreply.github.com>
2023-08-19 00:17:25 +0800

    Initial commit

---
+++ bluetix/src/main/java/bluetix/BluetixApplication.java
@@ -0,0 +1,13 @@
+package bluetix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluetixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluetixApplication.class, args);
+	}
+
+}
---
+++ bluetix/src/test/java/bluetix/BluetixApplicationTests.java
@@ -0,0 +1,13 @@
+package bluetix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
