a58eeb2e7c7c7448db260640557d8da3b2139496
Wong Boon Jhee <wbjhee@gmail.com>
2023-09-09 23:18:08 +0800

    removed authorization need for events and venues

--- bluetix/src/main/java/bluetix/security/SecurityConfiguration.java
+++ bluetix/src/main/java/bluetix/security/SecurityConfiguration.java
@@ -29,8 +29,10 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**")
-                        .permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/events/**").permitAll()
+                .requestMatchers("api/venues/**").permitAll()
+                .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
