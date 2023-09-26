f90b9347e15168df2afee5c276578b391c65f74d
Wong Boon Jhee <wbjhee@gmail.com>
2023-09-26 08:25:03 +0800

    removed target directory from tracking

---
+++ bluetix/src/test/java/bluetix/QueueServiceTest.java
@@ -0,0 +1,18 @@
+package bluetix;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import bluetix.service.QueuingService;
+
+public class QueueServiceTest {
+    @Test
+    public void queueProperty(){
+        QueuingService<Integer> queue = new QueuingService<>();
+        queue.enqueue(2);
+        queue.enqueue(3);
+        assertEquals(2, queue.dequeue());
+    }
+
+}
