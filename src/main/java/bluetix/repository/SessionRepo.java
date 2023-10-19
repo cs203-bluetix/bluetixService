18ea0a2421dc9aa0f51c35b2f0f47480c93e7c59
tkyj <82047680+tkyj@users.noreply.github.com>
2023-10-20 00:37:25 +0800

    Fixed sql for session Repo

--- src/main/java/bluetix/repository/SessionRepo.java
+++ src/main/java/bluetix/repository/SessionRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 
 import bluetix.model.Session;
 import bluetix.serializable.SessionId;
