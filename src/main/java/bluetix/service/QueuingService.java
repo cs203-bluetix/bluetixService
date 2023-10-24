aa284d21134bf56dcc6396d8413ceb324689c43b
Wong Boon Jhee <wbjhee@gmail.com>
2023-10-24 13:03:13 +0800

    added timestamping, fcfs after prioriy is considered

--- src/main/java/bluetix/service/QueuingService.java
+++ src/main/java/bluetix/service/QueuingService.java
@@ -27,7 +27,7 @@ public class QueuingService {
     public QueuingService(int ticketCount, UserRepo userRepo) {
         // Schedule the draining task to run every 5 seconds (adjust as needed)
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(this::drainQueue, 0, 5, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(this::drainQueue, 0, 1, TimeUnit.MINUTES);
         scheduler.scheduleAtFixedRate(this::kickIdleUsers, 0, 5, TimeUnit.SECONDS);
         // scheduler.scheduleAtFixedRate(this::kickFromQueue, 0, 5, TimeUnit.SECONDS);
         this.ticketCount = ticketCount;
