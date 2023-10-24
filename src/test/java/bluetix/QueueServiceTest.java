1efea3e99d5b9ce42fa77167508f4bd6067525f0
Wong Boon Jhee <wbjhee@gmail.com>
2023-10-24 11:28:07 +0800

    added ticket count

--- src/test/java/bluetix/QueueServiceTest.java
+++ src/test/java/bluetix/QueueServiceTest.java
@@ -9,7 +9,7 @@ import bluetix.service.QueuingService;
 public class QueueServiceTest {
     @Test
     public void queueProperty(){
-        QueuingService<Integer> queue = new QueuingService<>();
+        QueuingService<Integer> queue = new QueuingService<>(5);
         queue.enqueue(2);
         queue.enqueue(3);
         assertEquals(2, queue.dequeue());
